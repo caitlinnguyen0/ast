5d906d6f207abbf53eac9441341163c59f673b3b
caitlinnguyen0 <96394018+caitlinnguyen0@users.noreply.github.com>
2023-02-22 21:23:34 -0800

    Update prep_esc50.py

--- egs/esc50/prep_esc50.py
+++ egs/esc50/prep_esc50.py
@@ -36,9 +36,9 @@ def get_immediate_files(a_dir):
     return [name for name in os.listdir(a_dir) if os.path.isfile(os.path.join(a_dir, name))]
 
 # downlooad esc50
-# dataset provided in https://github.com/karolpiczak/ESC-50
+# dataset WWAS ORIGINALLY provided in https://github.com/karolpiczak/ESC-50, now provided in https://github.com/caitlinnguyen0/ESC-50
 if os.path.exists('./data/ESC-50-master') == False:
-    esc50_url = 'https://github.com/karoldvl/ESC-50/archive/master.zip'
+    esc50_url = 'https://github.com/caitlinnguyen0/ESC-50/archive/master.zip'
     wget.download(esc50_url, out='./data/')
     with zipfile.ZipFile('./data/ESC-50-master.zip', 'r') as zip_ref:
         zip_ref.extractall('./data/')
