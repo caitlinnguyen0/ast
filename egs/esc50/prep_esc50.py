121598c630f831e2d03ab1ee4e538288fd44d47e
caitlinnguyen0 <96394018+caitlinnguyen0@users.noreply.github.com>
2023-02-22 22:24:45 -0800

    Update prep_esc50.py

--- egs/esc50/prep_esc50.py
+++ egs/esc50/prep_esc50.py
@@ -66,7 +66,7 @@ if os.path.exists('./data/datafiles') == False:
 for fold in [1]:
     base_path = "./data/ESC-50-main/audio_16k/"
     meta = np.loadtxt('./data/ESC-50-main/meta/esc50.csv', delimiter=',', dtype='str', skiprows=1)
-    print(meta)
+    print(os.path.exists('./data/ESC-50-main/meta/esc50.csv'))
     train_wav_list = []
     eval_wav_list = []
     for i in range(0, len(meta)):
