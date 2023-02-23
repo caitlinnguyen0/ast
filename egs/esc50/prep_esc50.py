3ac27b2eb5e288490f9200ac9fdb28f494b7d07f
caitlinnguyen0 <96394018+caitlinnguyen0@users.noreply.github.com>
2023-02-22 22:12:12 -0800

    Update prep_esc50.py

--- egs/esc50/prep_esc50.py
+++ egs/esc50/prep_esc50.py
@@ -57,7 +57,7 @@ label_map = {}
 for i in range(1, len(label_set)):
     label_map[eval(label_set[i][2])] = label_set[i][0]
 print(label_map)
-print(label_map_
+print(label_map)
 
 # fix bug: generate an empty directory to save json files
 if os.path.exists('./data/datafiles') == False:
